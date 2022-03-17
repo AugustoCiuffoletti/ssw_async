b2cdf16a5591a30bc8dc4e4b9b48f052699367ba
AugustoCiuffoletti <augusto@di.unipi.it>
2022-03-17 11:54:11 +0100

    2 files modified

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # Async e await
 
-L'esempio dimostra il funzionamento di async e await. Modificare temporizzazione e ordine di esecuzione per capire meglio come funzionano.
+L'esempio dimostra il funzionamento di async e await.
+In questo branch le due promise vengono dichiarate nell'istruzione "await", quindi l'istruzione termina quando la promise si risolve, cioè dopo 3 e 5 secondi. Quindi la prima visualizzazione appare dopo tre secondi, la seconda dopo ulteriori 5 secondi. 
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-sswawait)
--- index.js
+++ index.js
@@ -4,16 +4,11 @@ const appDiv = document.getElementById("app");
 const outDiv = document.getElementById("output");
 appDiv.innerHTML = `<h1>Un esempio di <i>promise<i></h1>`;
 
-// Definisco una funzione sleep che restituisce una Promise
-// che si risolve dopo un certo intervallo parametrizzato
 function sleep(ms, txt) {
-  return new Promise(resolve => {
+  return new Promise( resolve => {
     setTimeout(() => resolve(txt), ms);
   });
 }
-// Definisco due promise per timeout e messaggi diversi
-// Lancio entrambi con await: vanno in parallelo
-// La funzione deve essere dichiarata async
 async function prova() {
   outDiv.innerHTML += await sleep(3000, "Fine primo step <br>");
   outDiv.innerHTML += await sleep(5000, "Fine secondo step <br>");
